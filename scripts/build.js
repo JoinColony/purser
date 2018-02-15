932618dc0386d2425f0dcc34fd568c74483dbb5d
Raul Glogovetan <raul@glogovetan.com>
2018-02-15 19:11:40 +0200

    Move: build jobs to separate script

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,13 @@
-const run = require('./utils').run;
-const paths = require('./paths');
+const jobs = require('./jobs');
 
-run(`rm -rf ${paths.lib}`, {}, 'Removed the \'lib\' folder for a clean build');
+jobs.clean('Removed the \'lib\' folder for a clean build');
 
-run('webpack', {}, 'Built UMD package for browsers');
+jobs.umd('Built UMD package for browsers');
 
-run('webpack --optimize-minimize', {}, 'Built minified UMD package for browsers');
+jobs.umdMinified('Built minified UMD package for browsers');
 
-run(`babel --out-dir ${paths.modules} ${paths.source}`, { BABEL_ENV: 'es' }, 'Built ES6 modules');
+jobs.esModules('Built ES6 modules');
 
-run(`flow-copy-source --ignore "**/__tests__/**" ${paths.source} ${paths.modules}`, {}, 'Exported Raw Flow types');
+jobs.flowTypes('Exported RAW Flow types');
 
-run(`babel --out-dir ${paths.lib} ${paths.source}`, { BABEL_ENV: 'cjs' }, 'Built CommonJS modules');
+jobs.cjsModules('Built CommonJS modules');
---
+++ scripts/jobs.js
@@ -0,0 +1,23 @@
+const run = require('./utils').run;
+const paths = require('./paths');
+
+const clean = message => run(`rm -rf ${paths.lib}`, {}, message);
+
+const umd = message => run('webpack', {}, message);
+
+const umdMinified = message => run('webpack --optimize-minimize', {}, message);
+
+const esModules = message => run(`babel --out-dir ${paths.modules} ${paths.source}`, { BABEL_ENV: 'es' }, message);
+
+const cjsModules = message => run(`babel --out-dir ${paths.lib} ${paths.source}`, { BABEL_ENV: 'cjs' }, message);
+
+const flowTypes = message => run(`flow-copy-source --ignore "**/__tests__/**" ${paths.source} ${paths.modules}`, {}, message);
+
+module.exports = {
+  clean,
+  umd,
+  umdMinified,
+  esModules,
+  cjsModules,
+  flowTypes,
+};
