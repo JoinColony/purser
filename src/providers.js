b9ad3c73c2892c8d3f0e33c6a756121aa59d0db5
Raul Glogovetan <raul@glogovetan.com>
2018-02-20 16:47:09 +0200

    Refactor: provider methods setup

--- src/providers.js
+++ src/providers.js
@@ -2,17 +2,29 @@
 
 import { providers } from 'ethers';
 
+const defaultNetwork = 'homestead';
+
+const etherscan = (network: string = defaultNetwork): void => {
+  const provider = new providers.EtherscanProvider(network);
+  return provider;
+};
+
+const infura = (network: string = defaultNetwork): void => {
+  const provider = new providers.InfuraProvider(network);
+  return provider;
+};
+
+const metamask = (network: string = defaultNetwork): void => {
+  if (window.web3 && window.web3.currentProvider) {
+    return new providers.Web3Provider(window.web3.currentProvider, network);
+  }
+  return undefined;
+};
+
 const colonyWallet = {
-  etherscan: (network: string = 'homested'): void =>
-    new providers.EtherscanProvider(network),
-  infura: (network: string = 'homested'): void =>
-    new providers.InfuraProvider(network),
-  metamask: (network: string = 'homested'): void => {
-    if (window.web3 && window.web3.currentProvider) {
-      return new providers.Web3Provider(window.web3.currentProvider, network);
-    }
-    return undefined;
-  },
+  etherscan,
+  infura,
+  metamask,
 };
 
 export default colonyWallet;
