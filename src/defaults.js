6401f0beffbb1d5130c57895502cc5c53385d6f5
Raul Glogovetan <raul@glogovetan.com>
2018-02-21 15:58:29 +0200

    Add: `defaults` helper

---
+++ src/defaults.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+export const DEFAULT_NETWORK = 'homestead';
+export const TEST_NETWORK = 'rinkeby';
+
+const defaults = {
+  DEFAULT_NETWORK,
+  TEST_NETWORK,
+};
+
+export default defaults;
