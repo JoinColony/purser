57fd8c9590ddc7b402d7ec1119b297ee5719cadf
Raul Glogovetan <raul@glogovetan.com>
2018-02-22 13:14:44 +0200

    Add: `infura` provider unit tests

--- src/__tests__/providers.test.js
+++ src/__tests__/providers.test.js
@@ -1,10 +1,10 @@
 import { providers } from 'ethers';
 
-import { etherscan } from '../providers';
+import { etherscan, infura } from '../providers';
 import { DEFAULT_NETWORK } from '../defaults';
 
 describe('`providers` module', () => {
-  describe('etherscan provider', () => {
+  describe('`etherscan` provider', () => {
     test('Connects with defaults', () => {
       providers.EtherscanProvider = jest.fn();
       etherscan();
@@ -30,6 +30,35 @@ describe('`providers` module', () => {
       const provider = etherscan(testNetworkName);
       expect(providers.EtherscanProvider).toHaveBeenCalled();
       expect(providers.EtherscanProvider).toThrow();
+      expect(provider).toEqual({});
+    });
+  });
+  describe('`infura` provider', () => {
+    test('Connects with defaults', () => {
+      providers.InfuraProvider = jest.fn();
+      infura();
+      expect(providers.InfuraProvider).toHaveBeenCalled();
+      expect(providers.InfuraProvider).toHaveBeenCalledWith(DEFAULT_NETWORK);
+    });
+    test('Connects with custom network and api key', () => {
+      providers.InfuraProvider = jest.fn();
+      const testNetworkName = 'skynet';
+      const testApiKey = '159346284575888';
+      infura(testNetworkName, testApiKey);
+      expect(providers.InfuraProvider).toHaveBeenCalled();
+      expect(providers.InfuraProvider).toHaveBeenCalledWith(
+        testNetworkName,
+        testApiKey,
+      );
+    });
+    test('Catch the connection error if something goes wrong', () => {
+      providers.InfuraProvider = jest.fn(() => {
+        throw new Error();
+      });
+      const testNetworkName = 'network-name-does-not-exist';
+      const provider = infura(testNetworkName);
+      expect(providers.InfuraProvider).toHaveBeenCalled();
+      expect(providers.InfuraProvider).toThrow();
       expect(provider).toEqual({});
     });
   });
