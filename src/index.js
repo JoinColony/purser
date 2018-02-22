291440fc4a509984b31d91240acd68daa6624ca7
Raul Glogovetan <raul@glogovetan.com>
2018-02-22 14:23:11 +0200

    Add: export top-level about object

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@
 
 import providers from './providers';
 import utils from './utils';
+import { ENV } from './defaults';
+import { name, version } from '../package.json';
 
 const colonyWallet = {
   utils,
   providers,
+  about: {
+    name,
+    version,
+    environment: ENV,
+  },
 };
 
 export default colonyWallet;
