01ab43d37e6388ba58a86748f50ea2988f64ad60
Raul Glogovetan <raul@glogovetan.com>
2018-02-26 13:43:05 +0200

    Export `ethers` when debugging

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 /* @flow */
 
+import ethers from 'ethers';
+
 import providers from './providers';
 import utils from './utils';
 import { ENV } from './defaults';
 import { name, version } from '../package.json';
 
-const colonyWallet = {
-  utils,
-  providers,
-  about: {
-    name,
-    version,
-    environment: ENV,
+const debug = {
+  debug: {
+    ethers,
   },
 };
+
+const colonyWallet = Object.assign(
+  {},
+  {
+    utils,
+    providers,
+    about: {
+      name,
+      version,
+      environment: ENV,
+    },
+  },
+  ENV === 'development' ? debug : {},
+);
 
 export default colonyWallet;
