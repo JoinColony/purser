db0639cdea648fbbe06d49483abd9ee45f592b2f
Raul Glogovetan <raul@glogovetan.com>
2018-02-28 13:46:12 +0200

    Add: more unit tests for software wallet `createLegacy`
    For the new error catching and features added

--- src/__tests__/softwareWallet.test.js
+++ src/__tests__/softwareWallet.test.js
@@ -2,6 +2,7 @@ import ethers from 'ethers';
 
 import { legacyCreate } from '../softwareWallet';
 import { localhost } from '../providers';
+import * as utils from '../utils';
 
 jest.mock('ethers', () => ({ Wallet: {} }));
 
@@ -18,6 +19,14 @@ describe('`software` wallet module', () => {
       expect(ethers.Wallet.createRandom).toHaveBeenCalled();
       expect(wallet).toHaveProperty('provider');
     });
+    test('Creates a new wallet when provider is set to a falsy value', () => {
+      ethers.Wallet.createRandom = jest.fn(() => ({}));
+      utils.warn = jest.fn();
+      const wallet = legacyCreate(null);
+      expect(ethers.Wallet.createRandom).toHaveBeenCalled();
+      expect(utils.warn).toHaveBeenCalled();
+      expect(wallet).not.toHaveProperty('provider');
+    });
     test('Creates a new wallet with manual entrophy', () => {
       ethers.Wallet.createRandom = jest.fn(() => ({}));
       const entrophy = new Uint8Array(100);
@@ -26,6 +35,27 @@ describe('`software` wallet module', () => {
       expect(ethers.Wallet.createRandom).toHaveBeenCalledWith({
         extraEntrophy: entrophy,
       });
+    });
+    test('Creates a new wallet when entrophy is set to a falsy value', () => {
+      ethers.Wallet.createRandom = jest.fn(() => ({}));
+      utils.warn = jest.fn();
+      legacyCreate(localhost(), null);
+      expect(utils.warn).toHaveBeenCalled();
+      expect(ethers.Wallet.createRandom).toHaveBeenCalled();
+      expect(ethers.Wallet.createRandom).toHaveBeenCalledWith();
+    });
+    test("Returns new wallet even when there's and error", () => {
+      ethers.Wallet.createRandom = jest.fn(() => ({}));
+      utils.warn = jest.fn(() => {
+        throw new Error();
+      });
+      utils.error = jest.fn();
+      const wallet = legacyCreate(null);
+      expect(utils.warn).toHaveBeenCalled();
+      expect(utils.error).toHaveBeenCalled();
+      expect(ethers.Wallet.createRandom).toHaveBeenCalled();
+      expect(ethers.Wallet.createRandom).toHaveBeenCalledWith();
+      expect(wallet).not.toHaveProperty('provider');
     });
   });
 });
