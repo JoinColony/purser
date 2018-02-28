7b7b94ef164704109e12e7b11a88705006bf185c
Raul Glogovetan <raul@glogovetan.com>
2018-02-28 16:50:09 +0200

    Add: software wallet `openWithPrivateKey` unit tests

--- src/__tests__/softwareWallet.test.js
+++ src/__tests__/softwareWallet.test.js
@@ -1,34 +1,44 @@
 import ethers from 'ethers';
 
-import { legacyCreate } from '../softwareWallet';
+import { legacyCreate, openWithPrivateKey } from '../softwareWallet';
 import { localhost } from '../providers';
 import * as utils from '../utils';
 
-jest.mock('ethers', () => ({ Wallet: {} }));
+jest.mock('ethers', () => {
+  const Wallet = jest.fn().mockImplementation(privatekey => {
+    if (privatekey === '0x0') {
+      throw new Error();
+    }
+    return this;
+  });
+  Wallet.createRandom = jest.fn(() => ({}));
+  return { Wallet };
+});
+
+jest.mock('../utils');
 
 describe('`software` wallet module', () => {
+  beforeEach(() => {
+    utils.warn.mockReset();
+    utils.error.mockReset();
+  });
   describe('`createLegacy` method', () => {
     test('Creates a new wallet by default', () => {
-      ethers.Wallet.createRandom = jest.fn(() => ({}));
       legacyCreate();
       expect(ethers.Wallet.createRandom).toHaveBeenCalled();
     });
     test('Creates a new wallet with a manual provider', () => {
-      ethers.Wallet.createRandom = jest.fn(() => ({}));
       const wallet = legacyCreate(localhost());
       expect(ethers.Wallet.createRandom).toHaveBeenCalled();
       expect(wallet).toHaveProperty('provider');
     });
     test('Creates a new wallet when provider is set to a falsy value', () => {
-      ethers.Wallet.createRandom = jest.fn(() => ({}));
-      utils.warn = jest.fn();
       const wallet = legacyCreate(null);
       expect(ethers.Wallet.createRandom).toHaveBeenCalled();
       expect(utils.warn).toHaveBeenCalled();
       expect(wallet).not.toHaveProperty('provider');
     });
     test('Creates a new wallet with manual entrophy', () => {
-      ethers.Wallet.createRandom = jest.fn(() => ({}));
       const entrophy = new Uint8Array(100);
       legacyCreate(localhost(), entrophy);
       expect(ethers.Wallet.createRandom).toHaveBeenCalled();
@@ -37,15 +47,15 @@ describe('`software` wallet module', () => {
       });
     });
     test('Creates a new wallet when entrophy is set to a falsy value', () => {
-      ethers.Wallet.createRandom = jest.fn(() => ({}));
-      utils.warn = jest.fn();
       legacyCreate(localhost(), null);
       expect(utils.warn).toHaveBeenCalled();
       expect(ethers.Wallet.createRandom).toHaveBeenCalled();
       expect(ethers.Wallet.createRandom).toHaveBeenCalledWith();
     });
     test("Returns new wallet even when there's and error", () => {
-      ethers.Wallet.createRandom = jest.fn(() => ({}));
+      /*
+       * A little try to simulate an error during creation
+       */
       utils.warn = jest.fn(() => {
         throw new Error();
       });
@@ -56,6 +66,31 @@ describe('`software` wallet module', () => {
       expect(ethers.Wallet.createRandom).toHaveBeenCalled();
       expect(ethers.Wallet.createRandom).toHaveBeenCalledWith();
       expect(wallet).not.toHaveProperty('provider');
+    });
+  });
+  describe('`openWithPrivateKey` method', () => {
+    test('Opens the wallet with the provided private key', () => {
+      const mockPrivateKey = '0x1';
+      const wallet = openWithPrivateKey(mockPrivateKey, localhost());
+      expect(wallet).toBeInstanceOf(ethers.Wallet);
+      expect(ethers.Wallet).toHaveBeenCalled();
+      expect(ethers.Wallet).toHaveBeenCalledWith(mockPrivateKey, localhost());
+    });
+    test('Opens the wallet without setting a provider', () => {
+      const mockPrivateKey = '0x1';
+      const wallet = openWithPrivateKey(mockPrivateKey, null);
+      expect(wallet).toBeInstanceOf(ethers.Wallet);
+      expect(ethers.Wallet).toHaveBeenCalled();
+      expect(ethers.Wallet).toHaveBeenCalledWith(mockPrivateKey);
+      expect(utils.warn).toHaveBeenCalled();
+    });
+    test('Recovers if there was an error opening the wallet', () => {
+      const mockPrivateKey = '0x0';
+      const wallet = openWithPrivateKey(mockPrivateKey, null);
+      expect(wallet).not.toBeInstanceOf(ethers.Wallet);
+      expect(ethers.Wallet).toHaveBeenCalled();
+      expect(utils.error).toHaveBeenCalled();
+      expect(wallet).toBeUndefined();
     });
   });
 });
