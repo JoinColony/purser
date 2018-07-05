6332632ee3a09988d0cc5844ad0ded63d676dd76
Raul Glogovetan <raul@glogovetan.com>
2018-07-05 15:55:23 +0300

    Add: trezor `messages` file

---
+++ src/trezor/messages.js
@@ -0,0 +1,8 @@
+/* @flow */
+/* eslint-disable max-len */
+
+const classMessages: Object = {
+  userCancelled: 'User cancelled the account export request (via the Prompt)',
+};
+
+export default classMessages;
