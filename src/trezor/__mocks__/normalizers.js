053fa8beb5c6c9b98de237e5c93e86d1314481ae
Raul Glogovetan <raul@glogovetan.com>
2018-07-16 16:42:31 +0300

    Add: `trezor` value normalizer methods mocks

---
+++ src/trezor/__mocks__/normalizers.js
@@ -0,0 +1,7 @@
+export const derivationPathNormalizer = jest.fn(value => value);
+
+export const multipleOfTwoHexValueNormalizer = jest.fn(value => value);
+
+export const addressNormalizer = jest.fn(value => value);
+
+export const hexSequenceNormalizer = jest.fn(value => value);
