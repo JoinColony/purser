ce540c637cf9c8fb9c2014ce38c3594fc4342089
Raul Glogovetan <raul@glogovetan.com>
2018-07-16 16:40:09 +0300

    Add: `bigNumber` util method mock

--- src/__mocks__/utils.js
+++ src/__mocks__/utils.js
@@ -4,6 +4,11 @@ export const objectToErrorString = jest.fn();
 
 export const getRandomValues = jest.fn(value => value);
 
+export const bigNumber = jest.fn(value => ({
+  value,
+  toString: () => value,
+}));
+
 const utils = {
   warning,
   getRandomValues,
