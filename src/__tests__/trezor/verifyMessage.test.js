2d3ddb1d281f609020384ded783ed81807985026
Raul Glogovetan <raul@glogovetan.com>
2018-07-16 17:25:16 +0300

    Add: `trezor` `verifyMessage` static method unit tests

--- src/__tests__/trezor/verifyMessage.test.js
+++ src/__tests__/trezor/verifyMessage.test.js
@@ -1,31 +1,33 @@
-import { signMessage } from '../../trezor/staticMethods';
+import { verifyMessage } from '../../trezor/staticMethods';
 import { payloadListener } from '../../trezor/helpers';
 import {
-  derivationPathValidator,
+  addressValidator,
   messageValidator,
+  hexSequenceValidator,
 } from '../../trezor/validators';
 import {
-  derivationPathNormalizer,
+  addressNormalizer,
   hexSequenceNormalizer,
 } from '../../trezor/normalizers';
 
-import { PAYLOAD_SIGNMSG } from '../../trezor/payloads';
+import { PAYLOAD_VERIFYMSG } from '../../trezor/payloads';
 
 jest.mock('../../trezor/helpers');
 jest.mock('../../trezor/validators');
 jest.mock('../../trezor/normalizers');
 
 describe('`Trezor` Hardware Wallet Module', () => {
-  describe('`signMessage()` static method', () => {
+  describe('`verifyMessage()` static method', () => {
     test('Uses the correct trezor service payload type', async () => {
-      const { type, requiredFirmware } = PAYLOAD_SIGNMSG;
-      await signMessage({
+      const { type, requiredFirmware } = PAYLOAD_VERIFYMSG;
+      await verifyMessage({
         /*
          * These values are not correct. Do not use the as reference.
          * If the validators wouldn't be mocked, they wouldn't pass.
          */
-        path: 0,
+        address: 0,
         mesasge: 0,
+        signature: 0,
       });
       expect(payloadListener).toHaveBeenCalled();
       expect(payloadListener).toHaveBeenCalledWith({
@@ -33,8 +35,9 @@ describe('`Trezor` Hardware Wallet Module', () => {
           /*
            * We only care about what payload type this method sends
            */
-          path: expect.anything(),
+          address: expect.anything(),
           message: expect.anything(),
+          signature: expect.anything(),
           /*
            * These two values should be correct
            */
@@ -43,61 +46,56 @@ describe('`Trezor` Hardware Wallet Module', () => {
         },
       });
     });
-    test('Validates message input values', async () => {
+    test('Validates message/signature input values', async () => {
       /*
        * These values are not correct. Do not use the as reference.
        * If the validators wouldn't be mocked, they wouldn't pass.
        */
-      const path = 'mocked-derivation-path';
+      const address = 'mocked-derivation-address';
       const message = 'mocked-message';
-      await signMessage({
-        path,
+      const signature = 'mocked-signature';
+      await verifyMessage({
+        address,
         message,
+        signature,
       });
       /*
-       * Validates the derivation path
+       * Validates the address
        */
-      expect(derivationPathValidator).toHaveBeenCalled();
-      expect(derivationPathValidator).toHaveBeenCalledWith(path);
+      expect(addressValidator).toHaveBeenCalled();
+      expect(addressValidator).toHaveBeenCalledWith(address);
       /*
        * Validates the message that is to be signed
        */
       expect(messageValidator).toHaveBeenCalled();
       expect(messageValidator).toHaveBeenCalledWith(message);
+      /*
+       * Validates the hex signature
+       */
+      expect(hexSequenceValidator).toHaveBeenCalled();
+      expect(hexSequenceValidator).toHaveBeenCalledWith(signature);
     });
-    test('Normalizes message input values', async () => {
+    test('Normalizes message/signature input values', async () => {
       /*
        * These values are not correct. Do not use the as reference.
        * If the validators wouldn't be mocked, they wouldn't pass.
        */
-      const path = 'mocked-derivation-path';
-      await signMessage({
-        path,
+      const address = 'mocked-derivation-address';
+      const signature = 'mocked-signature';
+      await verifyMessage({
+        address,
+        signature,
       });
       /*
-       * Normalizes the derivation path
+       * Normalizes the address
        */
-      expect(derivationPathNormalizer).toHaveBeenCalled();
-      expect(derivationPathNormalizer).toHaveBeenCalledWith(path);
-    });
-    test('Normalizes the return hex string', async () => {
-      const path = 'mocked-derivation-path';
-      const returnedHexString = '48656c6c6f20796f75';
+      expect(addressNormalizer).toHaveBeenCalled();
+      expect(addressNormalizer).toHaveBeenCalledWith(address, false);
       /*
-       * We're re-mocking the helpers just for this test so we can simulate
-       * a different response
-       */
-      payloadListener.mockImplementation(() => ({
-        signature: returnedHexString,
-      }));
-      await signMessage({
-        path,
-      });
-      /*
-       * Normalizes the return string
+       * Normalizes the signature
        */
       expect(hexSequenceNormalizer).toHaveBeenCalled();
-      expect(hexSequenceNormalizer).toHaveBeenCalledWith(returnedHexString);
+      expect(hexSequenceNormalizer).toHaveBeenCalledWith(signature, false);
     });
   });
 });
