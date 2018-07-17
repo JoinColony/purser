66ebafd0233880448a6ac3da85160494a488e8b5
Raul Glogovetan <raul@glogovetan.com>
2018-07-17 18:08:18 +0300

    Add: `trezor` address normalizer unit tests

---
+++ src/__tests__/trezor/normalizers/addressNormalizer.test.js
@@ -0,0 +1,20 @@
+import { addressNormalizer } from '../../../trezor/normalizers';
+
+jest.dontMock('../../../utils');
+jest.dontMock('../../../trezor/normalizers');
+
+const unPrefixedAddress = '586145EBa3A2545cac062EAd7DE0EC184A6C8Af5';
+const prefixedAddress = '0x586145EBa3A2545cac062EAd7DE0EC184A6C8Af5';
+
+describe('`Trezor` Hardware Wallet Module', () => {
+  describe('`addressNormalizer()` normalizer', () => {
+    test('Adds a prefix to an ethereum address', () => {
+      expect(addressNormalizer(unPrefixedAddress)).toEqual(prefixedAddress);
+    });
+    test('Removes prefix to an ethereum address', () => {
+      expect(addressNormalizer(prefixedAddress, false)).toEqual(
+        unPrefixedAddress,
+      );
+    });
+  });
+});
