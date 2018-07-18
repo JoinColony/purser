749dbff03719ae68e8ca091dca47cfcebaa18c7f
Raul Glogovetan <raul@glogovetan.com>
2018-07-18 11:31:56 +0300

    Refactor: split `utils` unit tests into multiple files

---
+++ src/__tests__/utils/verbose.test.js
@@ -0,0 +1,38 @@
+import utils from '../../utils';
+import * as defaults from '../../defaults';
+
+jest.dontMock('../../utils');
+
+global.console = {
+  warn: jest.fn(),
+  error: jest.fn(),
+};
+
+const { verbose } = utils;
+
+describe('`Utils` Core Module', () => {
+  describe('`verbose()` method', () => {
+    test('It should be verbose if the environment is not defined', () => {
+      defaults.ENV = undefined;
+      const isVerbose = verbose();
+      expect(isVerbose).toBeTruthy();
+    });
+    test("It should be verbose if we're in a development environment", () => {
+      defaults.ENV = 'development';
+      const isVerbose = verbose();
+      expect(isVerbose).toBeTruthy();
+    });
+    test(
+      "It should NOT be verbose if we're in a any kind of other" +
+        "environment (other than 'development')",
+      () => {
+        defaults.ENV = 'production';
+        let isVerbose = verbose();
+        expect(isVerbose).not.toBeTruthy();
+        defaults.ENV = 'testing';
+        isVerbose = verbose();
+        expect(isVerbose).not.toBeTruthy();
+      },
+    );
+  });
+});
---
+++ src/__tests__/utils/warning.test.js
@@ -0,0 +1,89 @@
+import { warning } from '../../utils';
+import * as defaults from '../../defaults';
+
+jest.dontMock('../../utils');
+
+global.console = {
+  warn: jest.fn(),
+  error: jest.fn(),
+};
+
+const message = 'This is a test message';
+
+describe('`Utils` Core Module', () => {
+  describe('`warning()` method', () => {
+    beforeEach(() => {
+      defaults.ENV = 'development';
+    });
+    afterEach(() => {
+      console.warn.mockClear();
+      console.error.mockClear();
+    });
+    test('It logs the correct message', () => {
+      warning(message);
+      expect(console.warn).toHaveBeenCalledWith(message);
+    });
+    test("It doesn't log a message when in production", () => {
+      defaults.ENV = 'production';
+      warning(message);
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(console.error).not.toHaveBeenCalled();
+      warning(message, { priority: 'high' });
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(console.error).not.toHaveBeenCalled();
+    });
+    test('It correctly sets the low priority', () => {
+      warning(message, { level: 'low' });
+      expect(console.warn).toHaveBeenCalled();
+      expect(console.error).not.toHaveBeenCalled();
+    });
+    test('It correctly sets the high priority', () => {
+      warning(message, { level: 'high' });
+      expect(console.error).toHaveBeenCalled();
+      expect(console.warn).not.toHaveBeenCalled();
+    });
+    test('It correctly falls back to the low priority', () => {
+      warning(message, { level: 'lower' });
+      expect(console.warn).toHaveBeenCalled();
+      expect(console.error).not.toHaveBeenCalled();
+      warning(message, { level: 123 });
+      expect(console.warn).toHaveBeenCalled();
+      expect(console.error).not.toHaveBeenCalled();
+      warning(message, { level: 'high', anotherProp: true });
+      expect(console.warn).toHaveBeenCalled();
+      expect(console.error).not.toHaveBeenCalled();
+    });
+    test("It doesn't log a message when in production", () => {
+      defaults.ENV = 'production';
+      warning(message);
+      expect(console.warn).not.toHaveBeenCalled();
+    });
+    test('It correctly splits out template literals', () => {
+      const templateLiterals = ['part', 'part', 'part'];
+      warning(
+        message,
+        templateLiterals[0],
+        templateLiterals[1],
+        templateLiterals[2],
+      );
+      expect(console.warn).toHaveBeenCalled();
+      expect(console.warn).toHaveBeenCalledWith(message, ...templateLiterals);
+    });
+    test('It correctly splits out template literals and priority', () => {
+      const templateLiterals = ['part', 'part', 'part'];
+      const priority = {
+        level: 'high',
+      };
+      warning(
+        message,
+        templateLiterals[0],
+        templateLiterals[1],
+        templateLiterals[2],
+        priority,
+      );
+      expect(console.error).toHaveBeenCalled();
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(console.error).toHaveBeenCalledWith(message, ...templateLiterals);
+    });
+  });
+});
