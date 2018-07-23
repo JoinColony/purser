6d6fae34914b1121b2b19e13e43f96e407b6f119
Raul Glogovetan <raul@glogovetan.com>
2018-07-23 17:12:05 +0300

    Fix: flow typed stub `hdkey`

--- flow-typed/hdkey.js
+++
@@ -1,9 +0,0 @@
-/*
- * Flow doesn't play well with `hdkey's` module export so we need to declare
- * it explicitly.
- *
- * See: https://github.com/facebook/flow/issues/2092
- */
-declare module 'hdkey' {
-  declare module.exports: any;
-}
