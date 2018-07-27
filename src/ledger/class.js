8355a59a580b9f0d8e91c48582336b72387e3a4d
Raul Glogovetan <raul@glogovetan.com>
2018-07-27 13:52:02 +0300

    Add: ledger `signTransaction` static method

--- src/ledger/class.js
+++ src/ledger/class.js
@@ -3,7 +3,12 @@
 import GenericWallet from '../core/genericWallet';
 import { DESCRIPTORS } from '../core/defaults';
 import { TYPE_HARDWARE, SUBTYPE_LEDGER } from '../core/types';
-import type { GenericClassArgumentsType } from '../core/flowtypes';
+import type {
+  GenericClassArgumentsType,
+  TransactionObjectType,
+} from '../core/flowtypes';
+
+import { signTransaction } from './staticMethods';
 
 const { WALLET_PROPS } = DESCRIPTORS;
 
@@ -16,6 +21,29 @@ export default class LedgerWallet extends GenericWallet {
        */
       type: Object.assign({}, { value: TYPE_HARDWARE }, WALLET_PROPS),
       subtype: Object.assign({}, { value: SUBTYPE_LEDGER }, WALLET_PROPS),
+      sign: Object.assign(
+        {},
+        {
+          value: async (transactionObject: TransactionObjectType) => {
+            /*
+             * For some reason prettier always suggests a way to fix this that would
+             * violate the 80 max-len rule. Wierd
+             */
+            /* eslint-disable prettier/prettier */
+            const {
+              chainId = (this.provider && this.provider.chainId) || 1,
+            } = transactionObject || {};
+            /* eslint-enable prettier/prettier */
+            return signTransaction(
+              Object.assign({}, transactionObject, {
+                derivationPath: await this.derivationPath,
+                chainId,
+              }),
+            );
+          },
+        },
+        WALLET_PROPS,
+      ),
     });
   }
 }
--- src/ledger/messages.js
+++ src/ledger/messages.js
@@ -3,7 +3,11 @@
 
 export const staticMethods: Object = {
   userExportGenericError:
-    'Could not export the wallet account, check the values you are sending to the Trezor service',
+    'Could not export the wallet account, check the values you are sending to your Ledger Wallet',
+  userSignTxGenericError:
+    'Could not sign the transaction, check the values you are sending to your Ledger Wallet',
+  userSignInteractionWarning:
+    'We need your confirmation before we can sign the transaction. Check your Ledger Wallet!',
 };
 
 const ledgerMessages = {
