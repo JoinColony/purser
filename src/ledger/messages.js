5d0ee82ae2f11f56c6ac2362f1aa29c236e07e99
Raul Glogovetan <raul@glogovetan.com>
2018-07-27 10:32:28 +0300

    Add: `ledger` wallet messages file

---
+++ src/ledger/messages.js
@@ -0,0 +1,13 @@
+/* @flow */
+/* eslint-disable max-len */
+
+export const staticMethods: Object = {
+  userExportGenericError:
+    'Could not export the wallet account, check the values you are sending to the Trezor service',
+};
+
+const ledgerMessages = {
+  staticMethods,
+};
+
+export default ledgerMessages;
