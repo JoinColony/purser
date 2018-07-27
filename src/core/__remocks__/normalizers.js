358033a637ca4dad6bb9d0d5e183b21ac4fd0d98
Raul Glogovetan <raul@glogovetan.com>
2018-07-27 12:33:52 +0300

    Fix: trezor `signTransaction` unit tests after refactor

--- src/core/__remocks__/normalizers.js
+++ src/core/__remocks__/normalizers.js
@@ -1,3 +1,9 @@
+/*
+ * The mocks folder had to be renamed in order for `jest` to not pick it up as duplicate haste mock file.
+ * See:
+ * https://github.com/facebook/jest/issues/2070
+ */
+
 export const derivationPathNormalizer = jest.fn(value => value);
 
 export const multipleOfTwoHexValueNormalizer = jest.fn(value => value);
