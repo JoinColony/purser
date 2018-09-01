593e488ae5044468fc856cf3523d4c52388036c4
Raul Glogovetan <raul@glogovetan.com>
2018-09-01 18:10:46 +0300

    Add: metamask class `signTransaction` validate input

--- src/metamask/class.js
+++ src/metamask/class.js
@@ -3,7 +3,10 @@
 import isEqual from 'lodash.isequal';
 
 import { warning } from '../core/utils';
-import { recoverPublicKey as recoverPublicKeyHelper } from '../core/helpers';
+import {
+  recoverPublicKey as recoverPublicKeyHelper,
+  userInputValidator,
+} from '../core/helpers';
 import { addressValidator, hexSequenceValidator } from '../core/validators';
 import { hexSequenceNormalizer } from '../core/normalizers';
 
@@ -14,7 +17,11 @@ import { signTransaction, signMessage, verifyMessage } from './staticMethods';
 import { methodCaller, setStateEventObserver } from './helpers';
 import { validateMetamaskState } from './validators';
 import { signMessage as signMessageMethodLink } from './methodLinks';
-import { PUBLICKEY_RECOVERY_MESSAGE, STD_ERRORS } from './defaults';
+import {
+  PUBLICKEY_RECOVERY_MESSAGE,
+  STD_ERRORS,
+  REQUIRED_PROPS as REQUIRED_PROPS_METAMASK,
+} from './defaults';
 import {
   MetamaskWallet as messages,
   staticMethods as staticMethodsMessages,
@@ -77,10 +84,23 @@ export default class MetamaskWallet {
       sign: Object.assign(
         {},
         {
-          value: async (transactionObject: TransactionObjectType) =>
-            signTransaction(
+          value: async (transactionObject: TransactionObjectType) => {
+            /*
+             * Validate the trasaction's object input
+             */
+            userInputValidator({
+              firstArgument: transactionObject,
+              /*
+               * @NOTE We're using the locally defined Required prop
+               * As opposed to the one imported from core, like we do for the
+               * other two methods
+               */
+              requiredAll: REQUIRED_PROPS_METAMASK.SIGN_TRANSACTION,
+            });
+            return signTransaction(
               Object.assign({}, transactionObject, { from: this.address }),
-            ),
+            );
+          },
         },
         WALLET_PROPS,
       ),
--- src/metamask/defaults.js
+++ src/metamask/defaults.js
@@ -7,3 +7,10 @@ export const STD_ERRORS: Object = {
   CANCEL_MSG_SIGN: 'User denied message signature',
   CANCEL_TX_SIGN: 'User denied transaction signature',
 };
+
+/*
+ * Prop names used to validate user input against
+ */
+export const REQUIRED_PROPS: Object = {
+  SIGN_TRANSACTION: ['to', 'value'],
+};
