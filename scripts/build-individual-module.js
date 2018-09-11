ca6a62a8e7c1af81b4d4a3329ead80dd72857706
Raul Glogovetan <raul@glogovetan.com>
2018-09-11 15:54:57 +0300

    Remove: library UMD builds

--- scripts/build-individual-module.js
+++ scripts/build-individual-module.js
@@ -34,74 +34,6 @@ const buildEs = (source, buildFolder, message) => run(
   message
 );
 
-/*
- * @TODO Move webpack build logic into `utils`
- */
-const buildUmd = (source, buildFolder, message, minimize = false) => {
-  /*
-   * @TODO Reduce wasted cycles
-   * There's an argument here that it will be better to just pass down the module name instead
-   * of reversing it from the folder's name...
-   */
-  const rootModulePath = path.resolve(source);
-  const moduleName = path.basename(rootModulePath);
-  const moduleFileName = camelCase(`colony-${moduleName}`);
-  return webpack(
-    {
-      entry: [source],
-      mode: BUILD_MODE,
-      output: {
-        filename: minimize ? `${moduleFileName}.min.js` : `${moduleFileName}.js`,
-        path: buildFolder,
-        library: moduleFileName,
-        libraryTarget: 'umd',
-        libraryExport: "default",
-      },
-      /*
-       * @TODO Fix `alias` workaround
-       *
-       * This is to do with the fact that we 'build' the final packages file before publishing,
-       * so webpack doesn't actually know where to import the `core` module from
-       */
-      resolve: {
-        alias: {
-          // '@colony/purser-core': path.resolve(MODULES, 'purser-core', 'src'),
-        },
-      },
-      module: {
-        rules: [
-          {
-            test: /\.js?$/,
-            /*
-             * Exclude JUST the top-level `node_modules`, otherwise our own folder
-             * structure will also be excluded
-             */
-            exclude: path.resolve('.', 'node_modules'),
-            use: {
-              loader: 'babel-loader',
-            },
-          },
-        ],
-      },
-      optimization: {
-        minimize: minimize ? true : false,
-      },
-    },
-    (err, stats) => {
-      if (err || stats.hasErrors()) {
-      // Handle errors here
-      }
-      console.log(message);
-    },
-  )
-};
-
-/*
- * Wrapper for the `buildUmd` so that we have a consistent message call signature
- */
-const buildMinifiedUmd = (source, buildFolder, message) =>
-  buildUmd(source, buildFolder, message, true);
-
 const buildIndividualModule = async (moduleName) => {
   const modulePath = path.resolve(MODULES, moduleName);
   const cjsBuildFolder = path.resolve(modulePath, FOLDERS.BUILD);
@@ -135,34 +67,6 @@ const buildIndividualModule = async (moduleName) => {
     esBuildFolder,
     `Building ${chalk.green(
       'ES Module'
-    )} for ${chalk.white('@colony/')}${chalk.whiteBright.bold(
-      moduleName
-    )}${chalk.white(' @ ')}${chalk.white.bold(
-      packageFile.version
-    )}`,
-  );
-  /*
-   * Build UMD Modules
-   */
-  buildUmd(
-    modulePath,
-    umdBuildFolder,
-    `Building ${chalk.green(
-      'UMD Module'
-    )} for ${chalk.white('@colony/')}${chalk.whiteBright.bold(
-      moduleName
-    )}${chalk.white(' @ ')}${chalk.white.bold(
-      packageFile.version
-    )}`,
-  );
-  /*
-   * Build Minified UMD Modules
-   */
-  buildMinifiedUmd(
-    modulePath,
-    umdBuildFolder,
-    `Building ${chalk.green(
-      'Minified UMD Module'
     )} for ${chalk.white('@colony/')}${chalk.whiteBright.bold(
       moduleName
     )}${chalk.white(' @ ')}${chalk.white.bold(
