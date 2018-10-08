4afd6fdd12508a4a89351a8eedbeb0835422df91
Raul Glogovetan <raul@glogovetan.com>
2018-10-08 20:29:00 +0300

    Fix: `purser-sorftware` `hdnode`, `json-wallet` mocks

--- modules/tests/mocks/ethers/wallet/index.js
+++ modules/tests/mocks/ethers/wallet/index.js
@@ -9,34 +9,10 @@ export const Wallet = jest.fn().mockImplementation(privateKey => {
 
 Wallet.createRandom = jest.fn(() => ({ privateKey: 'mocked-private-key' }));
 
-Wallet.isEncryptedWallet = jest.fn(() => true);
-
-Wallet.fromEncryptedWallet = jest.fn(() => ({
+Wallet.fromEncryptedJson = jest.fn(() => ({
   privateKey: 'mocked-private-key',
   address: 'mocked-address',
   mnemonic: 'mocked-mnemonic',
 }));
 
-export const HDNode = {
-  fromMnemonic: jest.fn(mnemonic => ({
-    derivePath: jest.fn(() => {
-      if (mnemonic === 'mocked-mnemonic') {
-        return { privateKey: 'mocked-private-key' };
-      }
-      return { privateKey: 'another-mocked-private-key' };
-    }),
-  })),
-  isValidMnemonic: jest.fn(mnemonic => {
-    if (mnemonic === 'mocked-mnemonic') {
-      return true;
-    }
-    return false;
-  }),
-};
-
-const ethersWallet = {
-  Wallet,
-  HDNode,
-};
-
-export default ethersWallet;
+export default Wallet;
