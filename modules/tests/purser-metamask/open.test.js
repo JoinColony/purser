56e8a71337ffd14eaef6253de97cbe369a89cabd
Raul Glogovetan <raul@glogovetan.com>
2018-10-21 23:23:24 +0300

    Fix: `purser-metamask` open() static method unit tests

--- modules/tests/purser-metamask/open.test.js
+++ modules/tests/purser-metamask/open.test.js
@@ -1,3 +1,6 @@
+import Web3Instance from 'web3';
+
+import { warning } from '@colony/purser-core/utils';
 import metamaskWallet from '@colony/purser-metamask';
 import MetamaskWalletClass from '@colony/purser-metamask/class';
 import {
@@ -8,6 +11,13 @@ import {
 jest.dontMock('@colony/purser-metamask');
 
 jest.mock('@colony/purser-metamask/class');
+jest.mock('web3');
+
+/*
+ * Mocked values
+ */
+const mockedAddress = 'mocked-address';
+const mockedEnableMethod = jest.fn(() => [mockedAddress]);
 
 /*
  * @TODO Fix manual mocks
@@ -17,17 +27,74 @@ jest.mock('@colony/purser-metamask/class');
 jest.mock('@colony/purser-metamask/helpers', () =>
   require('@mocks/purser-metamask/helpers'),
 );
+jest.mock('@colony/purser-core/utils', () =>
+  require('@mocks/purser-core/utils'),
+);
 
 describe('Metamask` Wallet Module', () => {
+  beforeEach(() => {
+    global.ethereum = {
+      enable: mockedEnableMethod,
+    };
+  });
+  afterEach(() => {
+    Web3Instance.mockClear();
+    warning.mockClear();
+    mockedEnableMethod.mockClear();
+  });
   describe('`open()` static method', () => {
-    test('Detect the inpage injected proxy before opening', async () => {
+    test('Start in EIP-1102 mode', async () => {
+      await metamaskWallet.open();
+      /*
+       * Enabled the account
+       */
+      expect(mockedEnableMethod).toHaveBeenCalled();
+      /*
+       * Created the Web3 Instance
+       */
+      expect(Web3Instance).toHaveBeenCalled();
+      expect(Web3Instance).toHaveBeenCalledWith(global.ethereum);
+      /*
+       * We did not warn the user, we're not in legacy mode
+       */
+      expect(warning).not.toHaveBeenCalled();
+    });
+    test('Start in Legacy mode', async () => {
+      global.ethereum = undefined;
+      global.web3 = {
+        currentProvider: {
+          enable: mockedEnableMethod,
+        },
+      };
+      await metamaskWallet.open();
+      /*
+       * We warn the user
+       */
+      expect(warning).toHaveBeenCalled();
+      /*
+       * Call the helper method
+       */
+      expect(getInpageProvider).toHaveBeenCalled();
+      /*
+       * Enabled the account
+       */
+      expect(mockedEnableMethod).toHaveBeenCalled();
+      /*
+       * Created the Web3 Instance
+       */
+      expect(Web3Instance).toHaveBeenCalled();
+      /*
+       * Call the helper method
+       */
+    });
+    test('Detect the inpage provider before opening', async () => {
       await metamaskWallet.open();
       /*
        * Call the helper method
        */
       expect(methodCaller).toHaveBeenCalled();
     });
-    test("Get the address value from Metamask's state", async () => {
+    test("Get the address from Metamask's state (legacy)", async () => {
       await metamaskWallet.open();
       /*
        * Call the helper method
@@ -36,15 +103,16 @@ describe('Metamask` Wallet Module', () => {
     });
     test('Open the wallet with defaults', async () => {
       await metamaskWallet.open();
-      const mockedMetamaskProvider = getInpageProvider();
       /*
-       * Instantiates the LedgerWallet class
+       * Call the helper method
+       */
+      expect(methodCaller).toHaveBeenCalled();
+      /*
+       * Instantiates the Metamask class
        */
       expect(MetamaskWalletClass).toHaveBeenCalled();
       expect(MetamaskWalletClass).toHaveBeenCalledWith({
-        address:
-          /* eslint-disable-next-line no-underscore-dangle */
-          mockedMetamaskProvider.publicConfigStore._state.selectedAddress,
+        address: mockedAddress,
       });
     });
   });
