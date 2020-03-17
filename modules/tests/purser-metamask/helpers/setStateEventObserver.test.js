ce051cb80e19a447e23b7536320b97ac1f64d936
Jarred Filmer <jarred.filmer@gmail.com>
2020-03-18 07:07:43 +1000

    Fix: Support new ethereum provider api 	- pursuer-metamask was failing on mobile 	- Mobile version of metamask had etherum provider with new api 	- Re-implemented code to get the address and listen for account changes

--- modules/tests/purser-metamask/helpers/setStateEventObserver.test.js
+++ modules/tests/purser-metamask/helpers/setStateEventObserver.test.js
@@ -2,31 +2,34 @@ import * as helpers from '@colony/purser-metamask/helpers';
 
 jest.dontMock('@colony/purser-metamask/helpers');
 
-/*
- * We just need this method mocked, but since it's declared in a module we
- * need to test we have do do this little go-around trick and use the default export
- *
- * See: https://github.com/facebook/jest/issues/936
- */
-helpers.default.getInpageProvider = jest.fn(() => global.web3.currentProvider);
-
 const { setStateEventObserver } = helpers;
 
-/*
- * Mock the global injected inpage provider
- */
-global.web3 = {
-  currentProvider: {
-    publicConfigStore: {
-      _events: {
-        update: [],
-      },
-    },
-  },
-};
-
 describe('Metamask` Wallet Module', () => {
-  describe('`setStateEventObserver()` helper method', () => {
+  describe('`setStateEventObserver()` helper method with old ethereum provider api', () => {
+    beforeAll(() => {
+      /*
+       * Mock the global injected in-page provider
+       */
+      global.web3 = {
+        currentProvider: {
+          publicConfigStore: {
+            _events: {
+              update: [],
+            },
+          },
+        },
+      };
+
+      /*
+       * We just need this method mocked, but since it's declared in a module we
+       * need to test we have do do this little go-around trick and use the default export
+       *
+       * See: https://github.com/facebook/jest/issues/936
+       */
+      helpers.default.getInpageProvider = jest.fn(
+        () => global.web3.currentProvider,
+      );
+    });
     afterEach(() => {
       helpers.default.getInpageProvider.mockClear();
       /*
@@ -48,6 +51,36 @@ describe('Metamask` Wallet Module', () => {
       expect(stateEvents.update).toHaveLength(0);
       setStateEventObserver(() => {});
       expect(stateEvents.update).toHaveLength(1);
+    });
+  });
+
+  describe('`setStateEventObserver()` helper method with new ethereum provider api', () => {
+    beforeAll(() => {
+      /*
+       * Mock the ethereum provider with the new state change api
+       */
+      helpers.default.getInpageProvider = jest.fn(() => global.ethereum);
+      global.ethereum = {
+        on: jest.fn(() => {}),
+      };
+    });
+    afterEach(() => helpers.default.getInpageProvider.mockClear());
+    test('Utilises the new method of listening for events if available', async () => {
+      /*
+       * If ethereum.on is available we should be using that to listen for account state change
+       * Attempt to listen for account changes
+       */
+      const myListener = () => {};
+      setStateEventObserver(myListener);
+
+      /*
+       * Expect the new function was called with our listening function
+       */
+      expect(global.ethereum.on).toHaveBeenCalledTimes(1);
+      expect(global.ethereum.on).toHaveBeenCalledWith(
+        'accountsChanged',
+        myListener,
+      );
     });
   });
 });
