bf83c5862a8b089ce4bb6b494b419ac61b6db170
Raul Glogovetan <raul@glogovetan.com>
2021-01-20 19:21:25 +0200

    Update: metamask detect helpers tests

--- packages/@purser/metamask/__tests__/helpers/detect.test.ts
+++ packages/@purser/metamask/__tests__/helpers/detect.test.ts
@@ -12,46 +12,70 @@ describe('Metamask` Wallet Module', () => {
       await expect(detect()).rejects.toThrow();
       await expect(detect()).rejects.toThrow(new Error(messages.noExtension));
     });
-    test('Checks if extension is unlocked', async () => {
+    test('Checks if provider is connected to chain', async () => {
       /*
        * Mock the `isUnlocked()` ethereum method
        */
-      const isUnlocked = jest.fn(async () => false);
+      const isConnected = jest.fn(() => false);
       testGlobal.ethereum = {
-        isUnlocked,
+        isConnected,
       };
       await expect(detect()).rejects.toThrow();
-      await expect(detect()).rejects.toThrow(new Error(messages.isLocked));
+      await expect(detect()).rejects.toThrow(new Error(messages.noProvider));
     });
-    test('Checks if extension is enabled', async () => {
+    test('Checks if the account is unlocked', async () => {
       /*
        * To reach this step, the extension already needs to be unlocked
        * (isUnlocked should return true)
        *
        * Mock the `isEnabled()` ethereum method
        */
-      const isUnlocked = jest.fn(async () => true);
-      const isEnabled = jest.fn(async () => false);
+      const isConnected = jest.fn(() => true);
+      const isUnlocked = jest.fn(async () => false);
       testGlobal.ethereum = {
-        isUnlocked,
-        isEnabled,
+        isConnected,
+        _metamask: {
+          isUnlocked,
+        },
       };
       await expect(detect()).rejects.toThrow();
-      await expect(detect()).rejects.toThrow(new Error(messages.notEnabled));
+      await expect(detect()).rejects.toThrow(new Error(messages.isLocked));
     });
-    test('Checks if the proxy has the in-page provider set', async () => {
-      testGlobal.ethereum = undefined;
-      await expect(detect()).rejects.toThrow(new Error(messages.noExtension));
+    test('Checks if we have permission to access the account', async () => {
+      /*
+       * To reach this step, the extension already needs to be unlocked
+       * (isUnlocked should return true)
+       *
+       * Mock the `isEnabled()` ethereum method
+       */
+      const request = jest.fn(async () => []);
+      const isConnected = jest.fn(() => true);
+      const isUnlocked = jest.fn(async () => true);
+      testGlobal.ethereum = {
+        isConnected,
+        request,
+        _metamask: {
+          isUnlocked,
+        },
+      };
+      await expect(detect()).rejects.toThrow();
+      await expect(detect()).rejects.toThrow(new Error(messages.notConnected));
     });
     test('Returns true if the extension is enabled', async () => {
       /*
        * Metamask is unlocked and enabled
        */
+      const request = jest.fn(async () => [
+        { parentCapability: 'eth_accounts' },
+      ]);
+      const isConnected = jest.fn(() => true);
       const isUnlocked = jest.fn(async () => true);
-      const isEnabled = jest.fn(async () => true);
       testGlobal.ethereum = {
-        isUnlocked,
-        isEnabled,
+        isConnected,
+        request,
+        _metamask: {
+          isUnlocked,
+        },
       };
       await expect(detect()).resolves.not.toThrow();
       const wasDetected = await detect();
